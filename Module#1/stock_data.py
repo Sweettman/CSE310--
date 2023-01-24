a277ff5c46363e6cb9c32ff7af4b771c47b02eab
Jose LeBaron <91554870+Sweettman@users.noreply.github.com>
2023-01-24 22:04:19 +0000

    added some references

--- Module#1/stock_data.py
+++ Module#1/stock_data.py
@@ -4,6 +4,8 @@ import yfinance as yf
 from datetime import datetime as dt
 plt.style.use('ggplot')
 
+#By using these libraries I am able to obtain data on any 
+#ticker symbol, and all the information that I could need
 msft = yf.Ticker('googl')
 stockinfo = msft.info
 
